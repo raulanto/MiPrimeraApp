31abaa0dc6e8d4faa36a1baf156c4cdb0c8bdbe4
raul <74162376+raulanto@users.noreply.github.com>
2023-10-04 10:09:58 -0600

    iconos

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,9 @@
+//code 
 import { Component } from '@angular/core';
+import axios from 'axios';
+import { LoadingController } from '@ionic/angular';
+import {InfiniteScrollCustomEvent} from '@ionic/angular'
+
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +11,45 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+	//Constru
+  constructor(private loadingCtrl: LoadingController) {}
+  //array
+  code: any = [];
+  //on init
+  ngOnInit() {
+    this.loadcode();
+  }
+  //funcion asincrona
+  async loadcode(event?: InfiniteScrollCustomEvent) {
+    const loading = await this.loadingCtrl.create({
+      message: 'Cargando',
+      spinner: 'bubbles',
+    });
+    await loading.present();
+    const response = await axios({
+      method: 'get',
+      url: 'http://attendancedb.test/code',
+      withCredentials: true,
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+      .then((response) => {
+        this.code = response.data;
+        event?.target.complete();
+        console.log(this.code);
+        
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    loading.dismiss();
+  }
 
-  constructor() {}
-
+//   onIonInfinite(ev:any) {
+//     this.loadteacher();
+//     setTimeout(() => {
+//         (ev as InfiniteScrollCustomEvent).target.complete();
+//     }, 500);
+// }
 }
